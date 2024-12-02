a740dcbfdab635fa96eacfb7ce5146e3bfe5cdb7
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-12-02 18:26:26 -0300

    feat: added stack machine

---
+++ lesson2/JavaCompiler/MaquinaPilha.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class MaquinaPilha {
+  public static void main(String args[]) throws IOException {
+    if (args[0] == null) {
+      System.err.println("Nenhum arquivo de origem inserido");
+    }
+
+    var memory = new Stack<Integer>();
+    
+    var arquivo = new BufferedReader(new FileReader(args[0]));
+    while (true) {
+      var read_line = arquivo.readLine();
+      var line = read_line.split(" ");
+      
+      // Only operand that has more than one parameter is the PUSH
+      if (line.length > 1 && line[0].equals("PUSH")){
+        memory.push(Integer.parseInt(line[1]));
+      }
+      else {
+        var v1 = memory.pop();
+        var v2 = memory.pop();
+        switch (line[0]) {
+          case "DIV":
+            memory.push(v1 / v2);
+            break;
+          case "SUM":
+            memory.push(v1 + v2);
+            break;
+          case "SUB":
+            memory.push(v1 - v2);
+            break;
+          case "MULT":
+            memory.push(v1 / v2);
+            break;
+          case "PRINT":
+            System.out.println(memory.pop());
+          default:
+            System.err.println("Operador inesperado: " + line[0]);
+            break;
+        }
+      }
+      arquivo.close();
+    }
+
+  }
+}
